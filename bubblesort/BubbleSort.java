5223d808b48d21339d25e673f76aa3d60c920680
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-09-07 18:28:33 -0400

    Create BubbleSort.java
    Bubble sort

---
+++ bubblesort/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int arr[]){
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped= false;
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j] < arr[j-1]){
+                    //swap j with j-1
+                    int tmp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=tmp;
+                    swapped=true;
+                }
+            }
+            if(!swapped) // if swappping is not happening that means array is sorted so break
+                break;
+
+        }
+    }
+}
