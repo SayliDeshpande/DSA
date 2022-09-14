c4d0771951a38bf981474af3e1958ad966c3a662
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-09-14 19:25:40 -0400

    Update SqrtNum.java

--- Maths for DS/SqrtNum.java
+++ Maths for DS/SqrtNum.java
@@ -7,12 +7,12 @@ public class SqrtNum {
     }
 
     private static int findsqrt(int n) {
-
-        int num=0;
-        int start=0;int end=n;
+        //Applying binary search
+       int num=0;
+        int start=1;int end=n; // start should start with 1 else runtime error : divide by zero
         while(start<=end){
             int mid= start+(end-start)/2;
-            if(n/mid>=mid){
+            if(n/mid>=mid){ // condition should be: number/mid>=mid not mid*mid>=num , latter gives wrong output for 1000000
                 start=mid+1;
                 num=mid;
             }else{
