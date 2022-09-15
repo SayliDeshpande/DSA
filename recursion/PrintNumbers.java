1e4c47fb6880fd2dc257b09060cbb951efcde192
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-09-15 16:33:15 -0400

    Update PrintNumbers.java

--- recursion/PrintNumbers.java
+++ recursion/PrintNumbers.java
@@ -1,8 +1,9 @@
 public class PrintNumbers {
     public static void main(String[] args) {
         int n=10;
-        fun(n);
-        funRev(n);
+       // fun(n);
+       // funRev(n);
+        funBoth(n);
     }
     private static void fun(int n){
         if(n==0){
@@ -18,5 +19,16 @@ public class PrintNumbers {
         }
         System.out.println(n);    // note the sequence of function call and print, this is printing in reverse
         funRev(n-1);
+    }
+    
+     private static void funBoth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);    // note the sequence of function call and print, this is printing both , sequencial and reverse
+        funBoth(n-1);
+        System.out.println(n);
+
+
     }
 }
