aed5930266615ccfdd6eaf2a0ee15e26fc57c6a6
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-09-15 18:02:28 -0400

    Update SumOfN.java

--- recursion/SumOfN.java
+++ recursion/SumOfN.java
@@ -2,6 +2,7 @@ public class SumOfN {
     public static void main(String[] args) {
         int n = 5;
         System.out.println(sum(n));     // sum of numbers of given n till 1
+         System.out.println(sumOfDigits(100001));   // sum of digits of input number
     }
 
     private static int sum(int n) {
@@ -9,5 +10,12 @@ public class SumOfN {
             return 1;
         else
             return n + sum(n - 1);
+    }
+    
+     private static int sumOfDigits(int n){
+        if(n==0)
+            return 0;
+        int digit = (n%10);
+        return digit + sumOfDigits(n/10);
     }
 }
