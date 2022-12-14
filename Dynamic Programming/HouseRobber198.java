45693401f3d3de79d959ae9346366967325f7e9c
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-12-14 09:15:32 -0500

    Create HouseRobber198.java
    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
    
    https://leetcode.com/problems/house-robber/description/

---
+++ Dynamic Programming/HouseRobber198.java
@@ -0,0 +1,35 @@
+public class HouseRobber198 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(rob(nums));
+    }
+
+    public static int rob(int[] nums) {
+      /*  int maxmoney = Integer.MIN_VALUE;
+
+        int i = 0;
+        while(i < nums.length){
+            int sum = 0;
+            for(int j = i ; j< nums.length ; j+=2){
+                sum += nums[j];
+            }
+            if (maxmoney < sum) maxmoney = sum;
+            i++;
+        }
+        return maxmoney; */
+        // dynamic programming  approach
+        int max = nums[0];
+        for(int i = 0 ; i < nums.length ; i++){
+            if(i > 2){
+                nums[i] = Math.max(nums[i - 2], nums[i - 3]) + nums[i];
+            }else if (i > 1){
+                nums[i] = nums[i - 2] + nums[i];
+            }
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+}
+
+// time complexity : O(N)
+// space complexity : O(1)
