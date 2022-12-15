18039d50f0f38bdb56824d78a8327af4d0aa5f0e
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-12-14 21:44:33 -0500

    Create NthTribonacciNumber.java
    The Tribonacci sequence Tn is defined as follows: 
    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
    Given n, return the value of Tn.
    Input: n = 4
    
    https://leetcode.com/problems/n-th-tribonacci-number/description/

---
+++ Dynamic Programming/NthTribonacciNumber.java
@@ -0,0 +1,15 @@
+class NthTribonacciNumber {
+    public int tribonacci(int n) {
+        
+       if(n < 2) return n;
+        int a = 0, b = 1, c = 1, d;
+        while(n > 2){
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+            n--;
+        }
+        return c;
+    }
+}
