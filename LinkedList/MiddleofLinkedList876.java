49f7be049bea2d99a0f9428b1a56e7060fe9359d
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2022-12-22 23:36:32 -0500

    Create MiddleofLinkedList876.java
    Given the head of a singly linked list, return the middle node of the linked list.
    head = [1,2,3,4,5] , head = [1,2,3,4,5,6]
    https://leetcode.com/problems/middle-of-the-linked-list/description/?envType=study-plan&id=level-1

---
+++ LinkedList/MiddleofLinkedList876.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MiddleofLinkedList876 {
+    public ListNode middleNode(ListNode head) {
+        //using two pointers
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next; //make fast pointer traverse twice as slow. When fast reaches the end of the list, slow must be in the middle.
+        }
+        return slow; // so return slow
+    }
+}
