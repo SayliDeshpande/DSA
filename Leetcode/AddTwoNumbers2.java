7c04f675a471d2dd13b310b585b4a91d3052e410
Sayali Deshpande <44116052+SayliDeshpande@users.noreply.github.com>
2023-01-25 16:10:00 -0500

    Create AddTwoNumbers2.java
    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
    
    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
    
    Input: l1 = [2,4,3], l2 = [5,6,4]
    Output: [7,0,8]
    Explanation: 342 + 465 = 807.
    Example 2:
    
    Input: l1 = [0], l2 = [0]
    Output: [0]
    Example 3:
    
    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
    Output: [8,9,9,9,0,0,0,1]
    
    Constraints:
    
    The number of nodes in each linked list is in the range [1, 100].
    0 <= Node.val <= 9
    It is guaranteed that the list represents a number that does not have leading zeros.

---
+++ Leetcode/AddTwoNumbers2.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       // to store resul, need a list
+       ListNode dummy = new ListNode(0); // initiate with any value, say 0
+       // because it is sum of numbers , need to take care about carry 
+       int carry = 0;
+        ListNode curr = dummy;
+       // iterate and sum the nides of gives lists
+       while(l1 != null || l2 != null || carry == 1){
+           int sum = 0; // to store total sum
+           if(l1 != null){
+               sum += l1.val;
+               l1 = l1.next;
+           }
+           if(l2 != null){
+               sum += l2.val;
+               l2 = l2.next;
+           }
+           sum += carry;
+           carry = sum / 10;
+           ListNode node = new ListNode(sum % 10);
+           curr.next = node;
+           curr = curr.next;
+       }
+       return dummy.next;
+    }
+}
+
+/*
+Time Complexity :- BigO(max(N, M)) where N is length of l1 & M is length of l2
+Space Complexity :- BigO(max(N,M))
+*/
